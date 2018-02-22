4ba87f4e43656a189045467f5ae9a4760e62a6d1
ANISSA <anissamiloudi@hotmail.nl>
2018-02-22 12:48:23 +0100

    start of the algo

--- src/components/AskQuestion.js
+++ src/components/AskQuestion.js
@@ -21,34 +21,6 @@ class AskQuestion extends PureComponent {
   if (rangeNumber > 88){
 
   }
-  randomStudent(length){
-    return Math.floor(Math.random() *length)
-  }
-
-  clickButton(students,event){
-    let type = this.whatType()
-    let selectedStudents = students.filter(s => s.lastReview === type)
-    let student =  selectedStudents[this.randomStudent(selectedStudents.length)]
-    this.setState({selectedStudent: student})
-  }
-
-  render() {
-    const students = this.props.students
-    return (
-      <div className="AskQuestion">
-        <RaisedButton
-          label="Ask a Question"
-          primary={true}
-          onClick={this.clickButton.bind(this,students)}
-          icon={<QuestionIcon />} />
-          <Paper>
-          <img src={this.state.selectedStudent.picture} className='questionPic' />
-          <p className={this.state.selectedStudent.lastReview}> {this.state.selectedStudent.name}</p>
-          </Paper>
-      </div>
-    )
-  }
-}
 
 
-export default connect(null, {  })(AskQuestion)
+export default AskQuestion
